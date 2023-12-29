3c0a198d882c73f3e495c38b354b3e3b22834a18
Matthew Reese <awesomepop012@hotmail.com>
2023-12-28 21:20:40 -0800

    Updated stuff

--- src/py_pubsub/py_pubsub/drive_input_publisher.py
+++ src/py_pubsub/py_pubsub/drive_input_publisher.py
@@ -32,25 +32,12 @@ class DriveInputPublisher(Node):
         gamepad = gamepad_input.getGamepad(self.GAMEPAD_INDEX)
 
         if gamepad is not None:
-            (ls_x, ls_y) = gamepad_input.getLeftStick(gamepad, self.GAMEPAD_DEADZONE)
-            (l2, r2) = gamepad_input.getTriggers(gamepad, self.GAMEPAD_DEADZONE)
+            (_, ls_y) = gamepad_input.getLeftStick(gamepad, self.GAMEPAD_DEADZONE)
+            (_, rs_y) = gamepad_input.getLeftStick(gamepad, self.GAMEPAD_DEADZONE)
 
-            max_vel = 1.0  # Maximum velocity of the motors for the drive system
+            pass
 
-            # Calculate turning radius
-            turn_radius = ls_x
-
-            # Calculate left and right wheel velocities
-            left_vel = max_vel * (r2 - l2)
-            right_vel = max_vel * (r2 - l2)
-
-            # Adjust wheel velocities based on turning radius
-            if turn_radius > 0:
-                left_vel *= (1 - turn_radius)
-            elif turn_radius < 0:
-                right_vel *= (1 + turn_radius)
-
-            self.msg.data = [left_vel, right_vel]
+            self.msg.data = [ls_y, rs_y]
             self.control_publisher.publish(self.msg)
             
     def configure_gamepad_input(self):
