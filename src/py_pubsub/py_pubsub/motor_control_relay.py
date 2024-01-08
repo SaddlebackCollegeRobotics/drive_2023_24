db45fe25082d320c1bba3a6763f5e05ce71a1a0d
Matt <awesomepop012@hotmail.com>
2024-01-08 01:15:35 +0000

    Updated motor controller to use new can interface. Removed old interface.

--- src/py_pubsub/py_pubsub/motor_control_relay.py
+++ src/py_pubsub/py_pubsub/motor_control_relay.py
@@ -3,7 +3,7 @@ from rclpy.node import Node
 from std_msgs.msg import Float64MultiArray
 
 # from .motor_controller import MotorControllerManager
-from .can_manager import ODriveCanManager
+from can_manager import OdriveCanManager
 from odrive.enums import AxisState
 
 from time import sleep
@@ -20,26 +20,13 @@ class MinimalPublisher(Node):
 
         # Set up motor controllers ---------------------------------------
 
-        INITIAL_MAX_SPEED = 5 # 30
+        self._max_velocity = 30
 
-        sleep(1)
-
-        self._manager = ODriveCanManager([0, 1, 2, 3])
+        self._manager = OdriveCanManager([0, 1, 2, 3])
         
         
-        self._manager.set_axis_state([0, 1, 2, 3], AxisState.CLOSED_LOOP_CONTROL)
-        self._manager._set_parameter(3, 'identify', True)
-        self._manager._send_command(
-                0,
-                'Get_Version'
-            )
-        version = self._manager._receive_command_data(
-                0,
-                'Get_Version',
-            )
-        print(version)
-        # self.motor_controller_manager.full_calibration_sequence(2)
-        # self.motor_controller_manager.full_calibration_sequence_all()
+        self._manager.set_axis_state_all(AxisState.CLOSED_LOOP_CONTROL)
+        self._manager.set_parameter([0, 2], 'identify', True)
 
     def control_input_callback(self, msg: Float64MultiArray):
 
@@ -48,8 +35,8 @@ class MinimalPublisher(Node):
         if abs(norm_vel_left) > 1 or abs(norm_vel_right) > 1:
             raise ValueError
 
-        self._manager.set_velocity([0, 1], norm_vel_left)
-        self._manager.set_velocity([2, 3], norm_vel_right)
+        self._manager.set_velocity([0, 1], norm_vel_left * self._max_velocity)
+        self._manager.set_velocity([2, 3], norm_vel_right * self._max_velocity)
 
         
 
