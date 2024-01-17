30a5a86a313a5c8b1d7c0de3327f93bb345b2abb
Matthew <awesomepop012@hotmail.com>
2024-01-16 17:38:46 -0800

    Updated relay to make use of new controller api.

--- src/py_pubsub/py_pubsub/motor_control_relay.py
+++ src/py_pubsub/py_pubsub/motor_control_relay.py
@@ -2,7 +2,7 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float64MultiArray
 
-from .motor_controller import MotorControllerManager, ODriveCanInterface
+from .motor_controller import MotorControllerManager, ODriveCanInterface, MotorController
 from odrive.enums import AxisState
 
 from time import sleep
@@ -35,7 +35,7 @@ class MinimalPublisher(Node):
         self._manager.add_motor_controller('back_right', 2, self._max_speed)
         self._manager.add_motor_controller('front_right', 3, self._max_speed)
         
-        self._manager.set_axis_state_all(AxisState.CLOSED_LOOP_CONTROL)
+        self._manager.for_each(MotorController.set_axis_state, AxisState.CLOSED_LOOP_CONTROL)
 
         # self.watchdog_thread = Thread(target=self.feed_watchdog_loop, args=(self._manager,))
         # self.watchdog_thread.loop = True
